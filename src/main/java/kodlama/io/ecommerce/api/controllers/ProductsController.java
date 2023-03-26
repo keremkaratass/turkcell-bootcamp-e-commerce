52e9493327d98d9cf9f444602c6fd111cbeb204f
keremkaratass <karatas.kerem2000@gmail.com>
2023-03-26 10:51:43 +0300

    from in-memoryrepository to jpa repository

--- src/main/java/kodlama/io/ecommerce/api/controllers/ProductsController.java
+++ src/main/java/kodlama/io/ecommerce/api/controllers/ProductsController.java
@@ -1,45 +1,39 @@
 package kodlama.io.ecommerce.api.controllers;
 
 import kodlama.io.ecommerce.business.abstracts.ProductService;
-import kodlama.io.ecommerce.entities.concretes.Product;
+import kodlama.io.ecommerce.entities.Product;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/products")
 public class ProductsController {
+
     private final ProductService service;
-
-    public ProductsController(ProductService service) {
-        this.service = service;
-    }
-
     @GetMapping
-    public List<Product> getAll() {
+    public List<Product> getAll(){
         return service.getAll();
     }
-
-    @GetMapping("{id}")
-    public Product getById(@PathVariable int id) {
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable int id){
         return service.getById(id);
     }
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product add(@RequestBody Product product) {
+    public Product add(@RequestBody Product product){
         return service.add(product);
     }
-
     @PutMapping("/{id}")
-    public Product update(@PathVariable int id, @RequestBody Product product) {
+    public Product update(@PathVariable int id,@RequestBody Product product){
         return service.update(id, product);
     }
-
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable int id) {
+    public void delete(@PathVariable int id){
         service.delete(id);
     }
 
--- src/main/java/kodlama/io/ecommerce/business/abstracts/ProductService.java
+++ src/main/java/kodlama/io/ecommerce/business/abstracts/ProductService.java
@@ -1,19 +1,15 @@
 package kodlama.io.ecommerce.business.abstracts;
 
-import kodlama.io.ecommerce.entities.concretes.Product;
-import kodlama.io.ecommerce.repository.abstracts.ProductRepository;
+import kodlama.io.ecommerce.entities.Product;
 
 import java.util.List;
 
 public interface ProductService {
     List<Product> getAll();
-
     Product getById(int id);
-
     Product add(Product product);
-
     Product update(int id, Product product);
-
     void delete(int id);
+
 
 }
